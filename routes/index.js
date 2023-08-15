bf2232228f48770474542f818c9e94b8280560c0
n0ll0 <101214789+n0ll0@users.noreply.github.com>
2023-08-15 20:06:15 +0300

    added post endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.MONGO_URI;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,6 +12,9 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const collection = client.db("veb").collection(Users);
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,13 +32,32 @@ async function run() {
 run();
 
 /* GET home page. */
-router.get("/signin", function (req, res, next) {
+router.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET home page. */
+router.get("/signin", function (req, res) {
   res.render("signin");
 });
 
 /* GET home page. */
-router.get("/signup", function (req, res, next) {
+router.get("/signup", function (req, res) {
   res.render("signup");
+});
+
+/* POST home page. */
+router.post("/signin", function (req, res) {
+  let formData = req.body;
+  console.log(formData);
+  res.render("index", { title: "Express" })
+});
+
+/* POST home page. */
+router.post("/signup", function (req, res) {
+  let formData = req.body;
+  console.log(formData);
+  res.render("index", { title: "Express" })
 });
 
 
